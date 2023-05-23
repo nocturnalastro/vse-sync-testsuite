c9e39be6f269c3994845f1046ed54f5c283f1198
Michele Costa <micosta@redhat.com>
2023-05-23 12:02:08 +0100

    Add tests for devices module

---
+++ pkg/collectors/devices/ptp_devices_test.go
@@ -0,0 +1,114 @@
+// Copyright 2023 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package devices_test
+
+import (
+	"fmt"
+	"net/url"
+	"strconv"
+	"strings"
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	"github.com/redhat-partner-solutions/vse-sync-testsuite/pkg/clients"
+	"github.com/redhat-partner-solutions/vse-sync-testsuite/pkg/collectors/devices"
+	"github.com/redhat-partner-solutions/vse-sync-testsuite/testutils"
+)
+
+var testPod = &v1.Pod{
+	ObjectMeta: metav1.ObjectMeta{
+		Name:        "TestPod-8292",
+		Namespace:   "TestNamespace",
+		Annotations: map[string]string{},
+	},
+}
+
+var _ = Describe("NewContainerContext", func() {
+	var clientset *clients.Clientset
+	var response map[string][]byte
+	BeforeEach(func() {
+		clientset = testutils.GetMockedClientSet(testPod)
+		response = make(map[string][]byte)
+		responder := func(method string, url *url.URL) ([]byte, []byte, error) {
+			cmd := url.Query()["command"]
+			return response[strings.Join(cmd, " ")], []byte(""), nil
+		}
+		clients.NewSPDYExecutor = testutils.NewFakeNewSPDYExecutor(responder, nil)
+	})
+
+	When("called GetPTPDeviceInfo", func() {
+		It("should return a valid PTPDeviceInfo", func() {
+			vendor := "0x8086"
+			devID := "0x1593"
+			gnssDev := "gnss0"
+
+			response["ls /sys/class/net/aFakeInterface/device/gnss/"] = []byte("gnss0")
+			response["cat /sys/class/net/aFakeInterface/device/device"] = []byte(devID)
+			response["cat /sys/class/net/aFakeInterface/device/vendor"] = []byte(vendor)
+
+			ctx, err := clients.NewContainerContext(clientset, "TestNamespace", "Test", "TestContainer")
+			Expect(err).NotTo(HaveOccurred())
+			info := devices.GetPTPDeviceInfo("aFakeInterface", ctx)
+			Expect(info.DeviceID).To(Equal(devID))
+			Expect(info.VendorID).To(Equal(vendor))
+			Expect(info.TtyGNSS).To(Equal("/dev/" + gnssDev))
+		})
+	})
+	When("called GetDevDPLLInfo", func() {
+		It("should return a valid PTPDeviceInfo", func() {
+			state := "10"
+			offset := "-34"
+
+			response["cat /sys/class/net/aFakeInterface/device/dpll_1_state"] = []byte(state)
+			response["cat /sys/class/net/aFakeInterface/device/dpll_1_offset"] = []byte(offset)
+
+			ctx, err := clients.NewContainerContext(clientset, "TestNamespace", "Test", "TestContainer")
+			Expect(err).NotTo(HaveOccurred())
+			info := devices.GetDevDPLLInfo(ctx, "aFakeInterface")
+			Expect(info.State).To(Equal(state))
+			Expect(info.Offset).To(Equal(offset))
+
+		})
+	})
+	When("called ReadTtyGNSS", func() {
+		It("should return a valid GNSSTTYLines", func() {
+			line := "definitely a line from a log"
+			nLines := 1
+			timeout := 10
+			devInfo := devices.PTPDeviceInfo{
+				TtyGNSS: "/dev/gnss0",
+			}
+			key := fmt.Sprintf("timeout %d head -n %s %s", timeout, strconv.Itoa(nLines), devInfo.TtyGNSS)
+
+			response[key] = []byte(line)
+
+			ctx, err := clients.NewContainerContext(clientset, "TestNamespace", "Test", "TestContainer")
+			Expect(err).NotTo(HaveOccurred())
+			GNSSLines := devices.ReadTtyGNSS(ctx, devInfo, nLines, timeout)
+			Expect(GNSSLines.TTY).To(Equal(devInfo.TtyGNSS))
+			Expect(GNSSLines.Lines).To(Equal(line))
+		})
+	})
+})
+
+func TestCommand(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Devices Suite")
+}
