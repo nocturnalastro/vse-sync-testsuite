1be747aa2e26cca7a2693bd10d425def758a62da
Michele Costa <micosta@redhat.com>
2024-01-26 11:21:05 +0000

    Move TGM collector using framework into subdir

---
+++ tgm-collector/pkg/collectors/collector.go
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+package collectors
+
+import (
+	"time"
+
+	"github.com/redhat-partner-solutions/vse-sync-collection-tools/tgm-collector/pkg/callbacks"
+	"github.com/redhat-partner-solutions/vse-sync-collection-tools/tgm-collector/pkg/clients"
+	"github.com/redhat-partner-solutions/vse-sync-collection-tools/tgm-collector/pkg/utils"
+)
+
+type Collector interface {
+	Start() error                                // Setups any internal state required for collection to happen
+	Poll(chan PollResult, *utils.WaitGroupCount) // Poll for collectables
+	CleanUp() error                              // Stops the collector and cleans up any internal state. It should result in a state that can be started again
+	GetPollInterval() time.Duration              // Returns the collectors polling interval
+	IsAnnouncer() bool
+}
+
+// A union of all values required to be passed into all constructions
+type CollectionConstructor struct {
+	Callback               callbacks.Callback
+	Clientset              *clients.Clientset
+	ErroredPolls           chan PollResult
+	PTPInterface           string
+	Msg                    string
+	LogsOutputFile         string
+	TempDir                string
+	PollInterval           int
+	DevInfoAnnouceInterval int
+	IncludeLogTimestamps   bool
+	KeepDebugFiles         bool
+}
+
+type PollResult struct {
+	CollectorName string
+	Errors        []error
+}
+
+type baseCollector struct {
+	callback     callbacks.Callback
+	ctx          clients.ExecContext
+	pollInterval time.Duration
+	isAnnouncer  bool
+	running      bool
+}
+
+func (base *baseCollector) GetPollInterval() time.Duration {
+	return base.pollInterval
+}
+
+func (base *baseCollector) IsAnnouncer() bool {
+	return base.isAnnouncer
+}
+
+func (base *baseCollector) Start() error {
+	return nil
+}
+
+func (base *baseCollector) CleanUp() error {
+	base.running = true
+	if base.ctx != nil {
+		c, ok := base.ctx.(*clients.ReusedConnectionContext)
+		if ok {
+			c.CloseShell()
+		}
+	}
+	return nil
+}
+
+func newBaseCollector(
+	pollInterval int,
+	isAnnouncer bool,
+	callback callbacks.Callback,
+	ctx clients.ExecContext,
+) *baseCollector {
+	return &baseCollector{
+		callback:     callback,
+		isAnnouncer:  isAnnouncer,
+		running:      false,
+		pollInterval: time.Duration(pollInterval) * time.Second,
+		ctx:          ctx,
+	}
+}
---
+++ tgm-collector/pkg/collectors/dpll_collector.go
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+package collectors
+
+import (
+	"fmt"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/redhat-partner-solutions/vse-sync-collection-tools/tgm-collector/pkg/collectors/contexts"
+	"github.com/redhat-partner-solutions/vse-sync-collection-tools/tgm-collector/pkg/collectors/devices"
+)
+
+const (
+	DPLLCollectorName = "DPLL"
+)
+
+// Returns a new DPLLCollector from the CollectionConstuctor Factory
+func NewDPLLCollector(constructor *CollectionConstructor) (Collector, error) {
+	ctx, err := contexts.GetPTPDaemonContext(constructor.Clientset)
+	if err != nil {
+		return &DPLLNetlinkCollector{}, fmt.Errorf("failed to create DPLLCollector: %w", err)
+	}
+	dpllFSExists, err := devices.IsDPLLFileSystemPresent(ctx, constructor.PTPInterface)
+	log.Debug("DPLL FS exists: ", dpllFSExists)
+	if dpllFSExists && err == nil {
+		return NewDPLLFilesystemCollector(constructor)
+	} else {
+		return NewDPLLNetlinkCollector(constructor)
+	}
+}
+
+func init() {
+	RegisterCollector(DPLLCollectorName, NewDPLLCollector, optional)
+}
